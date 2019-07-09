3c2ff928b54cb07a6648a607853c71952b0c6fdf
Shubham Gopale <shubhindia123@gmail.com>
2019-07-09 16:30:23 +0530

    add bash script for getting everything

---
+++ PHP_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Start the docker service
+service docker start
+
+#Get the docker configuration file from git
+wget https://raw.githubusercontent.com/shubhindia/php_container/master/docker-compose.yml
+
+#start the container
+docker-compose up -d
