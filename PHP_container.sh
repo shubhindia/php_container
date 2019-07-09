f081808dbfb8df415ca480f69251d76cd07d22bd
Shubham Gopale <shubhindia123@gmail.com>
2019-07-09 18:42:40 +0530

    Update PHP_container.sh

--- PHP_container.sh
+++ PHP_container.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Start the docker service
-sudo systemctl status docker 
-sudo service docker restart
+systemctl status docker 
+service docker restart
 
 #Get the docker configuration file from git
 curl https://raw.githubusercontent.com/shubhindia/php_container/master/docker-compose.yml > docker-compose.yml
 
 #sudo service docker start
 #start the container
-sudo docker-compose up -d
+docker-compose up -d
