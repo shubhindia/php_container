80df83549d151ea19ff55a91c808dc14e805d4ca
Shubham Gopale <shubhindia123@gmail.com>
2019-07-09 17:11:43 +0530

    try with sudo

--- PHP_container.sh
+++ PHP_container.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Start the docker service
-service docker start
+sudo service docker start
 
 #Get the docker configuration file from git
 curl https://raw.githubusercontent.com/shubhindia/php_container/master/docker-compose.yml > docker-compose.yml
 
-service docker start
+#sudo service docker start
 #start the container
-docker-compose up -d
+sudo docker-compose up -d
