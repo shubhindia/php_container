2704a06d4941e60457c777228785f0473a502130
Shubham Gopale <shubhindia123@gmail.com>
2019-07-09 16:32:34 +0530

    Update PHP_container.sh

--- PHP_container.sh
+++ PHP_container.sh
@@ -6,5 +6,6 @@ service docker start
 #Get the docker configuration file from git
 wget https://raw.githubusercontent.com/shubhindia/php_container/master/docker-compose.yml
 
+service docker start
 #start the container
 docker-compose up -d
